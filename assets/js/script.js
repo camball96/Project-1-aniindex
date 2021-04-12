0e435b962f53f1b861962b95d3cc928418b89f21
calebthecurious <calebthecurious@gmail.com>
2021-04-12 19:51:05 +1000

    Working API version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,25 @@
-
-searchAnime("gundam");
-
-
 const base_url = "https://api.jikan.moe/v3";
 
-function searchAnime(query) {
+function searchAnime() {
+  const query = document.getElementById("search").value;
+  console.log(query);
 
-    event.preventDefault();
-
-    const form = new FormData(this);
-    const query = form.get("search");
-
-    console.log(query);
-
-    fetch(`${base_url}/search/anime?q=${query}&page=1`)
+  fetch(`${base_url}/search/anime?q=${query}&page=1`)
     .then(response=>response.json())
     .then(updateDom)
     .catch(err=>console.e(err.message));
 }
 
 function updateDom(data){
-    data.results.forEach(anime=>console.log(data));
+    data.results.forEach(anime=>console.log(anime));
 }
 
-function pageLoaded(){
-    const form = document.getElementById('search_btn');
-    form.addEventListener("submit", searchAnime);
-}
+
+
+// function pageLoaded(){
+//     const form = document.getElementById('search_btn');
+//     form.addEventListener("submit", searchAnime);
+// }
 /*
 function searchAnime(query) {
     fetch("https://api.jikan.moe/v3/search/anime?q=" + query)
